be228ae11b8236f1b4361e51569f77b9e68b89d7
Aryan Biswas <aryanb198@gmail.com>
2024-05-07 15:24:42 +0530

    Time: 4 ms (73.05%) | Memory: 11.4 MB (27.75%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,53 @@
+// class Solution {
+// public:
+//     int f(int row, int col, vector<vector<int>> &dp, vector<vector<int>> &triangle){
+        
+//         if(row == triangle.size()-1){
+//             return triangle[row][col];
+//         }
+        
+//         if(dp[row][col] != 1e9){
+//             return dp[row][col];
+//         }
+        
+//         int down = triangle[row][col] + f(row+1, col, dp, triangle);
+//         int downRight = triangle[row][col] + f(row+1, col+1, dp, triangle);
+        
+//         return dp[row][col] = min(down, downRight);
+//     }
+    
+//     int minimumTotal(vector<vector<int>>& triangle) {
+        
+//         // Memoization
+//         int n = triangle.size();
+//         vector<vector<int>> dp(n, vector<int>(n, 1e9));
+        
+//         return f(0, 0, dp, triangle);
+//     }
+// };
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        // Tabulation
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, 1e9));
+        
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                
+                int down = triangle[i][j] + dp[i+1][j];
+                int downRight = triangle[i][j] + dp[i+1][j+1];
+                
+                dp[i][j] = min(down, downRight);
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
