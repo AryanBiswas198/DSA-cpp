bf477a0c9e715288653883b7e0336a1409d37470
Aryan Biswas <aryanb198@gmail.com>
2024-05-07 15:33:59 +0530

    Added space optimization

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -26,6 +26,7 @@
 //     }
 // };
 
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -49,5 +50,35 @@ public:
         }
         
         return dp[0][0];
+    }
+};
+
+
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        // Tabulation - Space Optimised
+        int n = triangle.size();
+        vector<int> dp(n, 1e9);
+        
+        for(int j=0; j<n; j++){
+            dp[j] = triangle[n-1][j];
+        }
+        
+        for(int i=n-2; i>=0; i--){
+            vector<int> temp(n, 1e9);
+            for(int j=i; j>=0; j--){
+                
+                int down = triangle[i][j] + dp[j];
+                int downRight = triangle[i][j] + dp[j+1];
+                
+                temp[j] = min(down, downRight);
+            }
+            dp = temp;
+        }
+        
+        return dp[0];
     }
 };
