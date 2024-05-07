8bccea29c97f3255b30dd919d581668538ee9231
Aryan Biswas <aryanb198@gmail.com>
2024-05-07 16:07:06 +0530

    Added Memoization, Tabulation and Space Optimization code

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -36,6 +36,46 @@
 //     }
 // };
 
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        // Tabulation
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 1e9));
+        
+        for(int j=0; j<m; j++){
+            dp[0][j] = matrix[0][j];
+        }
+        
+        for(int i=1; i<n; i++){
+            for(int j=0; j<m; j++){
+                int up = matrix[i][j], upLeft = 1e9, upRight = 1e9;
+                up += dp[i-1][j];
+                
+                if(j-1>=0){
+                    upLeft = matrix[i][j] + dp[i-1][j-1];
+                }
+                
+                if(j+1<m){
+                    upRight = matrix[i][j] + dp[i-1][j+1];
+                }
+                
+                dp[i][j] = min(up, min(upLeft, upRight));
+            }
+        }
+        
+        int mini = INT_MAX;
+        
+        for(int j=0; j<m; j++){
+            mini = min(mini, dp[n-1][j]);
+        }
+        return mini;
+    }
+};
+
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
