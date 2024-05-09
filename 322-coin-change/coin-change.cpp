e61c0866f18a5a4966cdbbd3390711f3b2e774ae
Aryan Biswas <aryanb198@gmail.com>
2024-05-09 17:43:55 +0530

    Time: 54 ms (18.42%) | Memory: 47.1 MB (25.28%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int f(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp){
+        
+        if(ind == 0){
+            if(amount % coins[ind] == 0){
+                return amount / coins[ind];
+            }
+            return 1e9;
+        }
+        
+        if(dp[ind][amount] != -1){
+            return dp[ind][amount];
+        }
+        
+        int notPick = 0 + f(ind-1, amount, coins, dp);
+        int pick = 1e9;
+        
+        if(coins[ind] <= amount){
+            pick = 1 + f(ind, amount-coins[ind], coins, dp);
+        }
+        
+        return dp[ind][amount] = min(pick, notPick);
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        
+        // Memoization
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        
+        int ans = f(n-1, amount, coins, dp);
+        
+        return ans >= 1e9 ? -1 : ans;
+    }
+};
