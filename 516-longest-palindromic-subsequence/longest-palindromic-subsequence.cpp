888b9de9b88e26a4a09327601da77b5a6ec9103e
Aryan Biswas <aryanb198@gmail.com>
2024-05-14 01:53:53 +0530

    Time: 65 ms (48.93%) | Memory: 73.8 MB (70.71%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -32,28 +32,55 @@
 // };
 
 
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+        
+//         // Tabulation
+//         string s2 = s;
+//         reverse(s2.begin(), s2.end());
+//         int n = s.size(), m = s2.size();
+        
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=m; j++){
+                
+//                 if(s[i-1] == s2[j-1]){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
+// };
+
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
         
-        // Tabulation
+        // Tabulation - space optimisation
         string s2 = s;
         reverse(s2.begin(), s2.end());
         int n = s.size(), m = s2.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<int> dp(m+1, 0);
         
         for(int i=1; i<=n; i++){
+            vector<int> temp(m+1, 0);
             for(int j=1; j<=m; j++){
-                
                 if(s[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    temp[j] = 1 + dp[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    temp[j] = max(dp[j], temp[j-1]);
                 }
             }
+            dp = temp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
