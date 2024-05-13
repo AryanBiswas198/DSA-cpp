7d428ab8380f011d998221321bbd37cde6227bf6
Aryan Biswas <aryanb198@gmail.com>
2024-05-14 02:44:47 +0530

    Time: 10 ms (71.76%) | Memory: 15.4 MB (42.69%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,30 +1,55 @@
+// class Solution {
+// public:
+//     int f(int ind1, int ind2, string &s1, string &s2, vector<vector<int>> &dp){
+        
+//         if(ind1 == 0 || ind2 == 0){
+//             return 0;
+//         }
+        
+//         if(dp[ind1][ind2] != -1){
+//             return dp[ind1][ind2];
+//         }
+        
+//         if(s1[ind1-1] == s2[ind2-1]){
+//             return dp[ind1][ind2] = 1 + f(ind1-1, ind2-1, s1, s2, dp);
+//         }
+//         else{
+//             return dp[ind1][ind2] = max(f(ind1-1, ind2, s1, s2, dp), f(ind1, ind2-1, s1, s2, dp));
+//         }
+//     }
+    
+//     int minDistance(string word1, string word2) {
+        
+//         // Memoization
+//         int n = word1.size(), m = word2.size();
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        
+//         int lcs = f(n, m, word1, word2, dp);
+//         return (n+m) - 2*lcs;
+//     }
+// };
+
+
 class Solution {
 public:
-    int f(int ind1, int ind2, string &s1, string &s2, vector<vector<int>> &dp){
-        
-        if(ind1 == 0 || ind2 == 0){
-            return 0;
-        }
-        
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
-        }
-        
-        if(s1[ind1-1] == s2[ind2-1]){
-            return dp[ind1][ind2] = 1 + f(ind1-1, ind2-1, s1, s2, dp);
-        }
-        else{
-            return dp[ind1][ind2] = max(f(ind1-1, ind2, s1, s2, dp), f(ind1, ind2-1, s1, s2, dp));
-        }
-    }
-    
     int minDistance(string word1, string word2) {
         
-        // Memoization
+        // Tabulation
         int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
         
-        int lcs = f(n, m, word1, word2, dp);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        int lcs = dp[n][m];
         return (n+m) - 2*lcs;
     }
 };
