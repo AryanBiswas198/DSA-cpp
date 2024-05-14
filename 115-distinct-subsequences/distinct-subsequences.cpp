4edaf81141567ad8b0cebfc28b3457340de1fc2d
Aryan Biswas <aryanb198@gmail.com>
2024-05-14 23:39:51 +0530

    Added Memoization, Tabulation and Space Optimised codes

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -58,3 +58,34 @@ public:
         return int(dp[n][m]);
     }
 };
+
+
+
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        
+        // Tabulation space optimisation
+        int n = s.size(), m = t.size();
+        vector<double> dp(m+1, 0);
+        
+        dp[0] = 1;
+        
+        for(int i=1; i<=n; i++){
+            vector<double> temp(m+1, 0);
+            temp[0] = 1;
+            for(int j=1; j<=m; j++){
+                
+                if(s[i-1] == t[j-1]){
+                    temp[j] = dp[j-1] + dp[j];
+                }
+                else{
+                    temp[j] = dp[j];
+                }
+            }
+            dp = temp;
+        }
+        return int(dp[m]);
+    }
+};
