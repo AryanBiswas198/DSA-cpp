0c5b242c10a966e2d2a2598eab3d82609272827e
Aryan Biswas <aryanb198@gmail.com>
2024-05-28 01:50:08 +0530

    Time: 590 ms (5.03%) | Memory: 221.2 MB (28.49%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -34,32 +34,66 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+        
+//         // Tabulation
+//         int n = prices.size(), k = 2;
+//         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        
+//         for(int i=n-1; i>=0; i--){
+//             for(int buy=0; buy<=1; buy++){
+//                 for(int k=1; k<=2; k++){
+//                     if(buy == 0){
+//                         int pick = -prices[i] + dp[i+1][buy+1][k];
+//                         int notPick = 0 + dp[i+1][buy][k];
+                        
+//                         dp[i][buy][k] = max(pick, notPick);
+//                     }
+//                     else{
+//                         int pick = prices[i] + dp[i+1][buy-1][k-1];
+//                         int notPick = 0 + dp[i+1][buy][k];
+                        
+//                         dp[i][buy][k] = max(pick, notPick);
+//                     }
+//                 }
+//             }
+//         }
+//         return dp[0][0][2];
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        // Tabulation
+        // Tabulation - space optimised
         int n = prices.size(), k = 2;
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        vector<vector<int>> dp(2, vector<int>(3, 0));
         
         for(int i=n-1; i>=0; i--){
+            vector<vector<int>> temp(2, vector<int>(3, 0));
             for(int buy=0; buy<=1; buy++){
                 for(int k=1; k<=2; k++){
+                    
                     if(buy == 0){
-                        int pick = -prices[i] + dp[i+1][buy+1][k];
-                        int notPick = 0 + dp[i+1][buy][k];
+                        int pick = -prices[i] + dp[buy+1][k];
+                        int notPick = 0 + dp[buy][k];
                         
-                        dp[i][buy][k] = max(pick, notPick);
+                        temp[buy][k] = max(pick, notPick);
                     }
                     else{
-                        int pick = prices[i] + dp[i+1][buy-1][k-1];
-                        int notPick = 0 + dp[i+1][buy][k];
+                       int pick = prices[i] + dp[buy-1][k-1];
+                       int notPick = 0 + dp[buy][k];
                         
-                        dp[i][buy][k] = max(pick, notPick);
+                       temp[buy][k] = max(pick, notPick); 
                     }
                 }
             }
+            dp = temp;
         }
-        return dp[0][0][2];
+        
+        return dp[0][2];
     }
 };
