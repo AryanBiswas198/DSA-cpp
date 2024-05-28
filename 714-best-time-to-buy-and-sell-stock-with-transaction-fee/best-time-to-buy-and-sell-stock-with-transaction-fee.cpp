755a08406347d223ecf8d14c9a76b3a2601ec11a
Aryan Biswas <aryanb198@gmail.com>
2024-05-28 13:13:28 +0530

    Time: 147 ms (46.81%) | Memory: 93.3 MB (32.91%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,35 +1,64 @@
+// class Solution {
+// public:
+//     int f(int ind, int buy, int fee, vector<int> &prices, vector<vector<int>> &dp){
+        
+//         if(ind >= prices.size()){
+//             return 0;
+//         }
+        
+//         if(dp[ind][buy] != -1){
+//             return dp[ind][buy];
+//         }
+        
+//         if(buy == 0){
+//             int pick = -prices[ind] + f(ind+1, buy+1, fee, prices, dp);
+//             int notPick = 0 + f(ind+1, buy, fee, prices, dp);
+            
+//             return dp[ind][buy] = max(pick, notPick);
+//         }
+//         else{
+//             int pick = prices[ind] - fee + f(ind+1, buy-1, fee, prices, dp);
+//             int notPick = 0 + f(ind+1, buy, fee, prices, dp);
+            
+//             return dp[ind][buy] = max(pick, notPick);
+//         }
+//     }
+    
+//     int maxProfit(vector<int>& prices, int fee) {
+        
+//         // Memoization
+//         int n = prices.size();
+//         vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        
+//         return f(0, 0, fee, prices, dp);
+//     }
+// };
+
 class Solution {
 public:
-    int f(int ind, int buy, int fee, vector<int> &prices, vector<vector<int>> &dp){
-        
-        if(ind >= prices.size()){
-            return 0;
-        }
-        
-        if(dp[ind][buy] != -1){
-            return dp[ind][buy];
-        }
-        
-        if(buy == 0){
-            int pick = -prices[ind] + f(ind+1, buy+1, fee, prices, dp);
-            int notPick = 0 + f(ind+1, buy, fee, prices, dp);
-            
-            return dp[ind][buy] = max(pick, notPick);
-        }
-        else{
-            int pick = prices[ind] - fee + f(ind+1, buy-1, fee, prices, dp);
-            int notPick = 0 + f(ind+1, buy, fee, prices, dp);
-            
-            return dp[ind][buy] = max(pick, notPick);
-        }
-    }
-    
     int maxProfit(vector<int>& prices, int fee) {
         
-        // Memoization
+        // Tabulation
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
         
-        return f(0, 0, fee, prices, dp);
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                
+                if(buy == 0){
+                    int pick = -prices[i] + dp[i+1][buy+1];
+                    int notPick = 0 + dp[i+1][buy];
+                    
+                    dp[i][buy] = max(pick, notPick);
+                }
+                else{
+                    int pick = prices[i] - fee + dp[i+1][buy-1];
+                    int notPick = 0 + dp[i+1][buy];
+                    
+                    dp[i][buy] = max(pick, notPick);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
