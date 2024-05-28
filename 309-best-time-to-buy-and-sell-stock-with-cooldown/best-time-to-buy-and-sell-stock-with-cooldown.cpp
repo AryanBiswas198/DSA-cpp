49988210ea8e3281b552d40343d718a65894ebf0
Aryan Biswas <aryanb198@gmail.com>
2024-05-28 13:06:51 +0530

    Time: 4 ms (54.12%) | Memory: 13.8 MB (70.00%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -34,31 +34,64 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+        
+//          // Tabulation
+//         int n = prices.size();
+//         vector<vector<int>> dp(n+2, vector<int>(2, 0));
+        
+//         for(int i=n-1; i>=0; i--){
+//             for(int buy=0; buy<=1; buy++){
+                
+//                 if(buy == 0){
+//                     int pick = -prices[i] + dp[i+1][buy+1];
+//                     int notPick = 0 + dp[i+1][buy];
+                    
+//                     dp[i][buy] = max(pick, notPick);
+//                 }
+//                 else{
+//                     int pick = prices[i] + dp[i+2][buy-1];
+//                     int notPick = 0 + dp[i+1][buy];
+                    
+//                     dp[i][buy] = max(pick, notPick);
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-         // Tabulation
+           // Tabulation - space optimisation
         int n = prices.size();
-        vector<vector<int>> dp(n+2, vector<int>(2, 0));
+        vector<int> prev1(2, 0); // Tracks 1 index behind
+        vector<int> prev2(2, 0); // Tracks 2 indexes behind
         
         for(int i=n-1; i>=0; i--){
+            vector<int> curr(2, 0);
             for(int buy=0; buy<=1; buy++){
                 
                 if(buy == 0){
-                    int pick = -prices[i] + dp[i+1][buy+1];
-                    int notPick = 0 + dp[i+1][buy];
+                    int pick = -prices[i] + prev1[buy+1];
+                    int notPick = 0 + prev1[buy];
                     
-                    dp[i][buy] = max(pick, notPick);
+                    curr[buy] = max(pick, notPick);
                 }
                 else{
-                    int pick = prices[i] + dp[i+2][buy-1];
-                    int notPick = 0 + dp[i+1][buy];
+                    int pick = prices[i] + prev2[buy-1];
+                    int notPick = 0 + prev1[buy];
                     
-                    dp[i][buy] = max(pick, notPick);
+                    curr[buy] = max(pick, notPick);
                 }
             }
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[0][0];
+        return prev1[0];
     }
 };
