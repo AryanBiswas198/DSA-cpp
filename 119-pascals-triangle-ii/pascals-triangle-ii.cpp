f17ccd2c711ff1a43676407ab595f3ba14fcadff
Aryan Biswas <aryanb198@gmail.com>
2024-06-22 14:41:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (24.64%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        // Brute Force
+        vector<vector<int>> arr(rowIndex+1);
+        vector<int> ans;
+        
+        for(int i=0; i<=rowIndex; i++){
+            arr[i].resize(i+1);
+            arr[i][0] = arr[i][i] = 1;
+            
+            for(int j=1; j<i; j++){
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+        
+        for(int i=0; i<=rowIndex; i++){
+            ans.push_back(arr[rowIndex][i]);
+        }
+        return ans;
+    }
+};
