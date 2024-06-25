db31a07e431826bc3350adda6e999c133798eeeb
Aryan Biswas <aryanb198@gmail.com>
2024-06-25 14:33:38 +0530

    Time: 26 ms (9.89%) | Memory: 27.9 MB (12.08%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size(), target = n/2;
+        unordered_map<int, int> mpp;
+        
+        for(auto it: nums){
+            mpp[it]++;
+            
+            if(mpp[it] > target){
+                return it;
+            }
+        }
+        return -1;
+    }
+};
