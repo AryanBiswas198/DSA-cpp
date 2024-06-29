fe0c501f5f159da80c0c502e3cb1320f04d8faf0
Aryan Biswas <aryanb198@gmail.com>
2024-06-30 01:05:32 +0530

    Time: 7 ms (21.89%) | Memory: 12 MB (10.30%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -16,34 +16,61 @@
 // };
 
 
+// class Solution {
+// public:
+//     bool searchEle(vector<int> matrix, int target){
+        
+//         int low = 0, high = matrix.size()-1;
+        
+//         while(low <= high){
+//             int mid = low + (high-low)/2;
+//             if(matrix[mid] == target){
+//                 return true;
+//             }
+//             else if(matrix[mid] < target){
+//                 low = mid + 1;
+//             }
+//             else{
+//                 high = mid-1;
+//             }
+//         }
+//         return false;
+//     }
+    
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+//         // Optimal Approach. TC: O(n + log(m)) SC: O(1)
+//         int n = matrix.size(), m = matrix[0].size();
+//         for(int i=0; i<n; i++){
+//             if(matrix[i][0] <= target && target <= matrix[i][m-1]){
+//                 return searchEle(matrix[i], target);
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
-    bool searchEle(vector<int> matrix, int target){
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int low = 0, high = matrix.size()-1;
+        // Best Approach - TC: O(log(m*n)). SC: O(1)
+        
+        int n = matrix.size(), m = matrix[0].size();
+        int low = 0, high = n*m-1;
         
         while(low <= high){
             int mid = low + (high-low)/2;
-            if(matrix[mid] == target){
+            
+            int nrow = mid / m, ncol = mid % m;
+            if(matrix[nrow][ncol] == target){
                 return true;
             }
-            else if(matrix[mid] < target){
+            else if(matrix[nrow][ncol] < target){
                 low = mid + 1;
             }
             else{
-                high = mid-1;
-            }
-        }
-        return false;
-    }
-    
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        // Optimal Approach. TC: O(n + log(m)) SC: O(1)
-        int n = matrix.size(), m = matrix[0].size();
-        for(int i=0; i<n; i++){
-            if(matrix[i][0] <= target && target <= matrix[i][m-1]){
-                return searchEle(matrix[i], target);
+                high = mid - 1;
             }
         }
         return false;
