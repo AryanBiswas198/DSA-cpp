9d2cd6579901976a1b7f8141119959bd25c8091e
Aryan Biswas <aryanb198@gmail.com>
2024-06-30 00:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (49.12%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,14 +1,49 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+//         // Brute Force - TC: O(n^2). SC: O(1)
+//         int n = matrix.size(), m = matrix[0].size();
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(matrix[i][j] == target){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
+    bool searchEle(vector<int> matrix, int target){
+        
+        int low = 0, high = matrix.size()-1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(matrix[mid] == target){
+                return true;
+            }
+            else if(matrix[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return false;
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        // Brute Force - TC: O(n^2). SC: O(1)
+        // Optimal Approach. TC: O(n + log(m)) SC: O(1)
         int n = matrix.size(), m = matrix[0].size();
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+            if(matrix[i][0] <= target && target <= matrix[i][m-1]){
+                return searchEle(matrix[i], target);
             }
         }
         return false;
