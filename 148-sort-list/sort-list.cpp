a533e31247dabcb3d9e61a17710a150736bab9f4
Aryan Biswas <aryanb198@gmail.com>
2024-07-09 16:15:23 +0530

    Time: 128 ms (55.38%) | Memory: 73.4 MB (39.44%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,27 +10,74 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+    ListNode *getMid(ListNode *head){
         
-        // Basic Approach
+        ListNode *slow = head, *fast = head->next;
         
-        vector<int> arr;
-        ListNode *temp = head;
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    
+    ListNode *sort(ListNode *left, ListNode *right){
+        
+        if(left == NULL){
+            return right;
         }
         
-        sort(arr.begin(), arr.end());
+        if(right == NULL){
+            return left;
+        }
         
         ListNode *ansHead = new ListNode(-1);
         ListNode *ansTail = ansHead;
         
-        for(auto it: arr){
-            ListNode *newNode = new ListNode(it);
-            ansTail->next = newNode;
+        while(left != NULL && right != NULL){
+            if(left->val < right->val){
+                ansTail->next = left;
+                ansTail = ansTail->next;
+                left = left->next;
+            }
+            else{
+                ansTail->next = right;
+                ansTail = ansTail->next;
+                right = right->next;
+            }
+        }
+        
+        while(left != NULL){
+            ansTail->next = left;
             ansTail = ansTail->next;
+            left = left->next;
+        }
+        
+        while(right != NULL){
+            ansTail->next = right;
+            ansTail = ansTail->next;
+            right = right->next;
         }
         return ansHead->next;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        
+        // Optimal Approach
+        
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        
+        ListNode *mid = getMid(head);
+        ListNode *left = head;
+        ListNode *right = mid->next;
+        mid->next = NULL;
+        
+        left = sortList(left);
+        right = sortList(right);
+        
+        ListNode *res = sort(left, right);
+        return res;
     }
 };
