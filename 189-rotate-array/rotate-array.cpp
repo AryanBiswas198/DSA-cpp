def3acdf352581e172d87479fd39ad7ef6714a6a
Aryan Biswas <aryanb198@gmail.com>
2025-02-01 23:23:51 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (40.32%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+       if(k == 0) {
+        return ;
+       }
+
+       k %= nums.size();
+
+       reverse(nums.begin(), nums.end());
+       reverse(nums.begin(), nums.begin()+k);
+       reverse(nums.begin()+k, nums.end());
+
+
+    }
+};
