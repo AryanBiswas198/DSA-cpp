36c4fb15b897709ba8fc598aa8353e60c89c08a2
Aryan Biswas <aryanb198@gmail.com>
2025-02-01 17:13:31 +0530

    Time: 14 ms (23.11%) | Memory: 7.7 MB (86.80%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rec(int n ) {
+        if(n == 0) {
+            return 0;
+        }
+
+        if(n == 1) {
+            return 1;
+        }
+
+        return rec(n-2) + rec(n-1);
+    }
+    int fib(int n) {
+        
+        return rec(n);
+    }
+};
