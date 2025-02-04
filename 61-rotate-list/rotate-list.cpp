60374a0c02710379aefc0994418c9d113dfe539a
Aryan Biswas <aryanb198@gmail.com>
2025-02-04 17:16:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (93.84%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -12,37 +12,28 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         
-        if(head == NULL || k == 0){
+        if(head == NULL || head->next == NULL || k == 0) {
             return head;
         }
-        
-        deque<int> dq;
-        int len = 0, cnt = 0;
+
         ListNode *temp = head;
-        
-        while(temp != NULL){
-            dq.push_back(temp->val);
-            len++;
+        int cnt = 1;
+
+        while(temp->next != NULL) {
             temp = temp->next;
-        }
-        
-        while(cnt < (k%len)){
-            int val = dq.back();
-            dq.pop_back();
-            dq.push_front(val);
             cnt++;
         }
-        
-        ListNode *ansHead = new ListNode(-1);
-        ListNode *ansTail = ansHead;
-        
-        while(!dq.empty()){
-            int val = dq.front();
-            dq.pop_front();
-            ListNode *newNode = new ListNode(val);
-            ansTail->next = newNode;
-            ansTail = ansTail->next;
+
+        temp->next = head;
+        k = k%cnt;
+        int len = cnt - k;
+
+        while(len--) {
+            temp = temp->next;
         }
-        return ansHead->next;
+
+        head = temp->next;
+        temp->next = NULL;
+        return head;
     }
 };
