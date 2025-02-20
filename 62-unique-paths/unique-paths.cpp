025e3cb5c9c042a0c9387d8b736e29994ed186ae
Aryan Biswas <aryanb198@gmail.com>
2025-02-20 21:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (56.67%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        
-        // Tabulation
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        
-        for(int row=0; row<m; row++){
-            for(int col=0; col<n; col++){
-                if(row == 0 && col == 0){
-                    dp[row][col] = 1;
-                }
-                else{
-                    int left = 0, up = 0;
-                    if(col > 0){
-                        left = dp[row][col-1];
-                    }
-                    
-                    if(row > 0){
-                        up = dp[row-1][col];
-                    }
-                    
-                    dp[row][col] = left + up;
-                }
-            }
+    int findUnique(int row, int col, vector<vector<int>> &dp) {
+        if(row < 0 || col < 0) {
+            return 0;
         }
-        return dp[m-1][n-1];
+
+        if(row == 0 && col == 0) {
+            return 1;
+        }
+
+        if(dp[row][col] != -1) {
+            return dp[row][col];
+        }
+
+        int left = findUnique(row, col-1, dp);
+        int up = findUnique(row-1, col, dp);
+
+        return dp[row][col] = (left + up);
+    }
+
+    int uniquePaths(int m, int n) {
+        // Memoization
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return findUnique(m-1, n-1, dp);
     }
 };
