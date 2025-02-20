936aef1943a8d7f4d077a58aafcd7aa4068b8185
Aryan Biswas <aryanb198@gmail.com>
2025-02-20 21:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (6.18%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    int f(vector<int> nums){
-        
+// Tabulation
+    int findMax(vector<int> nums) {
         int n = nums.size();
         vector<int> dp(n, -1);
-        
-        dp[0] = nums[0], dp[1] = 0;
-        
-        for(int i=1; i<n; i++){
-            int pick = nums[i];
-            if(i > 1){
-                pick = nums[i] + dp[i-2];
-            }
+        dp[0] = nums[0];
+
+        for(int i=1; i<n; i++) {
             int notPick = 0 + dp[i-1];
-            
+            int pick = nums[i];
+            if(i > 1) {
+                pick += dp[i-2];
+            }
             dp[i] = max(pick, notPick);
         }
         return dp[n-1];
     }
-    
+
     int rob(vector<int>& nums) {
-        
-        // Tabulation - as memoization doesn't works
         int n = nums.size();
-        if(n == 1){
+        if(n == 1) {
             return nums[0];
         }
-        
-        if(n == 2){
+        if(n == 2) {
             return max(nums[0], nums[1]);
         }
-        
-        vector<int> dp(n, -1);
+
         vector<int> temp1, temp2;
-        
-         for(int i=0; i<n; i++){
-             if(i!=0) temp1.push_back(nums[i]);
-             
-             if(i!=n-1) temp2.push_back(nums[i]);
-         }
-        
-        return max(f(temp1), f(temp2));
+        for(int i=0; i<n; i++) {
+            if(i != 0) temp1.push_back(nums[i]);
+            if(i != n-1) temp2.push_back(nums[i]);
+        }
+
+        return max(findMax(temp1), findMax(temp2));
     }
 };
