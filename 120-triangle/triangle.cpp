bb33e6620eb19f01833ccd833dd327b9e40be335
Aryan Biswas <aryanb198@gmail.com>
2025-02-21 02:32:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (46.81%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -19,9 +19,22 @@ public:
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
-        // Memoization
+        // Tabulation
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, 1e9));
-        return findMini(0, 0, dp, triangle);
+
+        for(int i=0; i<n; i++) {
+            dp[n-1][i] = triangle[n-1][i];
+        }
+
+        for(int i=n-2; i>=0; i--) {
+            for(int j=i; j>=0; j--) {
+                int down = triangle[i][j] + dp[i+1][j];
+                int downRight = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down, downRight);
+            }
+        }
+        return dp[0][0];
     }
 };
