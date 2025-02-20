b482a6604f4c10e5745dd25bc7af72eecb7a13e3
Aryan Biswas <aryanb198@gmail.com>
2025-02-20 15:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.90%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    int f(int ind, vector<int> &dp, vector<int> nums){
-        
-        if(ind < 0){
+    int findMax(int ind, vector<int> &dp, vector<int> &nums) {
+
+        if(ind < 0) {
             return 0;
         }
-        
-        if(ind == 0){
+
+        if(ind == 0) {
             return nums[ind];
         }
-        
-        if(dp[ind] != -1){
+
+        if(dp[ind] != -1) {
             return dp[ind];
         }
-        
-        int notPick = 0 + f(ind-1, dp, nums);
-        int pick = nums[ind] + f(ind-2, dp, nums);
-        
+
+        int notPick = 0 + findMax(ind-1, dp, nums);
+        int pick = nums[ind] + findMax(ind-2, dp, nums);
+
         return dp[ind] = max(pick, notPick);
     }
-    
+
     int rob(vector<int>& nums) {
-        
-        // Memoization
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        
-        int ans = f(n-1, dp, nums);
-        return ans;
+        vector<int> dp(n, -1);
+        return findMax(n-1, dp, nums);
     }
 };
