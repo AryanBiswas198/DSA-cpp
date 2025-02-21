241aa8f8be9bf618fafbd02875106372028cf43d
Aryan Biswas <aryanb198@gmail.com>
2025-02-21 17:12:40 +0530

    Time: 99 ms (71.83%) | Memory: 100.4 MB (38.10%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,7 @@
 class Solution {
 public:
-    bool checkIf(int ind, int target, vector<vector<int>> &dp, vector<int> &nums) {
-        if(target == 0) {
-            return true;
-        }
-
-        if(ind == 0) {
-            return target == nums[ind];
-        }
-
-        if(dp[ind][target] != -1) {
-            return dp[ind][target];
-        }
-
-        int notPick = checkIf(ind-1, target, dp, nums);
-        int pick = false;
-        if(nums[ind] <= target) {
-            pick = checkIf(ind-1, target-nums[ind], dp, nums);
-        }
-
-        return dp[ind][target] = pick || notPick;
-    }
-
     bool canPartition(vector<int>& nums) {
-        // Memoization
+        // Tabulation
         int n = nums.size(), sum = 0, k;
         if(n == 1) {
             return false;
@@ -33,13 +11,33 @@ public:
             sum += it;
         }
 
-        k = sum / 2;
-
         if(sum % 2 == 1) {
             return false;
         }
 
-        vector<vector<int>> dp(n, vector<int>(k+1, -1));
-        return checkIf(n-1, k, dp, nums);
+
+        k = sum / 2;
+        vector<vector<int>> dp(n, vector<int>(k+1, 0));
+        for(int i=0; i<n; i++) {
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= k) {
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int target=1; target<=k; target++) {
+                bool notPick = dp[i-1][target];
+                bool pick = false;
+
+                if(nums[i] <= target) {
+                    pick = dp[i-1][target-nums[i]];
+                }
+
+                dp[i][target] = pick || notPick;
+            }
+        }
+        return dp[n-1][k];
     }
 };
