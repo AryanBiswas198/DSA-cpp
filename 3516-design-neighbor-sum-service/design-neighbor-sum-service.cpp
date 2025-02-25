443484864d5dee1cfd28b0a7107abe26c745057e
Aryan Biswas <aryanb198@gmail.com>
2025-02-25 19:16:45 +0530

    Time: 30 ms (11.54%) | Memory: 73 MB (46.15%) - LeetSync

--- 3516-design-neighbor-sum-service/design-neighbor-sum-service.cpp
+++ 3516-design-neighbor-sum-service/design-neighbor-sum-service.cpp
@@ -6,11 +6,26 @@ int n, m;
     NeighborSum(vector<vector<int>>& grid) {
         mat = grid;
         n = grid.size(), m = grid[0].size();
+        
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 mpp[grid[i][j]] = {i, j};
             }
         }
+    }
+
+    int findTotalSum(int row, int col, int drow[], int dcol[]) {
+        int sum = 0;
+        for(int i=0; i<4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+
+            if(nrow < 0 || ncol < 0 || nrow >= n || ncol >= m)  {
+                continue;
+            }
+            sum += mat[nrow][ncol];
+        }
+        return sum;
     }
     
     int adjacentSum(int value) {
@@ -23,17 +38,7 @@ int n, m;
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
 
-        int sum = 0;
-        for(int i=0; i<4; i++) {
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-
-            if(nrow < 0 || ncol < 0 || nrow >= n || ncol >= m)  {
-                continue;
-            }
-            sum += mat[nrow][ncol];
-        }
-        return sum;
+        return findTotalSum(row, col, drow, dcol);
     }
     
     int diagonalSum(int value) {
@@ -45,17 +50,7 @@ int n, m;
         int drow[] = {-1, -1, 1, 1};
         int dcol[] = {-1, 1, -1, 1};
 
-        int sum = 0;
-        for(int i=0; i<4; i++) {
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-
-            if(nrow < 0 || ncol < 0 || nrow >= n || ncol >= m)  {
-                continue;
-            }
-            sum += mat[nrow][ncol];
-        }
-        return sum;
+        return findTotalSum(row, col, drow, dcol);
     }
 };
 
