9e198d9852ee5bb026f452695e16975ab87fb8d5
Aryan Biswas <aryanb198@gmail.com>
2025-05-27 15:25:55 +0530

    Time: 6 ms (12.03%) | Memory: 17.3 MB (12.68%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,67 +1,70 @@
 class Solution {
 public:
     bool isValid(int row, int col, int n, int m) {
-        if(row<0 || col<0 || row>=n || col>=m) {
+        if(row < 0 || col < 0 || row >= n || col >= m) {
             return false;
         }
         return true;
     }
 
-    void bfs(vector<vector<char>>& board, vector<vector<bool>> &vis, queue<pair<int, int>> &q) {
-        int n = board.size(), m = board[0].size();
+
+    void dfs(int row, int col, vector<vector<bool>> &vis, vector<vector<char>>& board) {
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
+        vis[row][col] = true;
 
-        while(!q.empty()) {
-            auto top = q.front();
-            q.pop();
-            
-            int row = top.first, col = top.second;
-            for(int i=0; i<4; i++) {
-                int nrow = row + drow[i], ncol = col + dcol[i];
+        for(int i=0; i<4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
 
-                if(isValid(nrow, ncol, n, m) && board[nrow][ncol] == 'O' && !vis[nrow][ncol]) {
-                    vis[nrow][ncol] = true;
-                    q.push({nrow, ncol});
-                }
+            if(isValid(nrow, ncol, board.size(), board[0].size()) && board[nrow][ncol] == 'O' && !vis[nrow][ncol]) {
+                dfs(nrow, ncol, vis, board);
             }
         }
     }
 
     void solve(vector<vector<char>>& board) {
+        
         int n = board.size(), m = board[0].size();
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
         vector<vector<bool>> vis(n, vector<bool>(m, 0));
         queue<pair<int, int>> q;
 
-        for(int row=0; row<n; row++) {
-            if(board[row][0] == 'O') {
-                q.push({row, 0});
-                vis[row][0] = true;
-            }
-            if(board[row][m-1] == 'O') {
-                q.push({row, m-1});
-                vis[row][m-1] = true;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if((i == 0 || j == 0 || i == n-1 || j == m-1) && board[i][j] == 'O' && !vis[i][j]) {
+                    dfs(i, j, vis, board);
+                }
             }
         }
-
-        for(int col=0; col<m; col++) {
-            if(board[0][col] == 'O') {
-                q.push({0, col});
-                vis[0][col] = true;
-            }
-
-            if(board[n-1][col] == 'O') {
-                q.push({n-1, col});
-                vis[n-1][col] = true;
-            }
-        }
-
-        bfs(board, vis, q);
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(board[i][j] == 'O' && !vis[i][j]) {
-                    board[i][j] = 'X';
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while(!q.empty()) {
+            auto front = q.front();
+            q.pop();
+
+            int row = front.first, col = front.second;
+            if(vis[row][col] == true) {
+                continue;
+            }
+            board[row][col] = 'X';
+            vis[row][col] = true;
+
+            for(int i=0; i<4; i++) {
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if(isValid(nrow, ncol, n, m) && board[nrow][ncol] == 'O' && !vis[nrow][ncol]) {
+                    q.push({nrow, ncol});
                 }
             }
         }
