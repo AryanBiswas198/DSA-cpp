787b5fade78aac6cd35647326ff16021a261e10b
Aryan Biswas <aryanb198@gmail.com>
2025-06-01 02:44:30 +0530

    Time: 64 ms (5.24%) | Memory: 53.1 MB (5.91%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
         
+        int mod = 1e9+7;
         unordered_map<long long, list<vector<long long>>> adj;
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
         for(auto it: roads) {
-            long long u = it[0], v = it[1], w = it[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
         }
+
+        dist[0] = 0;
+        ways[0] = 1;
 
         priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
         pq.push({0, 0});
 
-        vector<long long> dist(n, LLONG_MAX);
-        dist[0] = 0;
-
-        vector<int> ways(n, 0);
-        ways[0] = 1;
-
-        int mod = 1e9+7;
-
         while(!pq.empty()) {
-            auto top = pq.top();
+            auto front = pq.top();
             pq.pop();
 
-            long long dis = top.first, node = top.second;
+            long long dis = front.first, node = front.second;
+
             for(auto it: adj[node]) {
-                long long adjNode = it[0], edgeW = it[1];
-                if(dis + edgeW < dist[adjNode]) {
-                    dist[adjNode] = dis + edgeW;
+                long long adjNode = it[0], adjDis = it[1];
+                if(dis + adjDis < dist[adjNode]) {
+                    dist[adjNode] = dis + adjDis;
                     pq.push({dist[adjNode], adjNode});
                     ways[adjNode] = ways[node];
                 }
-                else if(dis + edgeW == dist[adjNode]) {
+                else if(dis + adjDis == dist[adjNode]) {
                     ways[adjNode] = (ways[node] + ways[adjNode])%mod;
                 }
             }
