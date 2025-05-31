0e8b0f8a4aa6a6f994b7b671b3b7f861e15b7759
Aryan Biswas <aryanb198@gmail.com>
2025-06-01 00:20:28 +0530

    Time: 53 ms (62.56%) | Memory: 24.4 MB (77.15%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool isValid(int row, int col, int n, int m) {
-        if(row<0 || col<0 || row>=n || col>=m) {
+        if(row < 0 || col < 0 || row >= n || col >= m) {
             return false;
         }
         return true;
     }
 
     int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int n = heights.size(), m = heights[0].size();
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
-        int n = heights.size(), m = heights[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+
+        dist[0][0] = 0;
         pq.push({0, {0, 0}});
 
         while(!pq.empty()) {
-            auto top = pq.top();
+            auto front = pq.top();
             pq.pop();
 
-            int dis = top.first, row = top.second.first, col = top.second.second;
+            int dis = front.first, row = front.second.first, col = front.second.second;
+
             if(row == n-1 && col == m-1) {
                 return dis;
             }
@@ -27,11 +32,12 @@ public:
             for(int i=0; i<4; i++) {
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
+
                 if(isValid(nrow, ncol, n, m)) {
-                    int maxEffort = max(abs(heights[nrow][ncol] - heights[row][col]), dis);
-                    if(maxEffort < dist[nrow][ncol]) {
-                        pq.push({maxEffort, {nrow, ncol}});
-                        dist[nrow][ncol] = maxEffort;
+                    int diff = max(dis, abs(heights[nrow][ncol] - heights[row][col]));
+                    if(diff < dist[nrow][ncol]) {
+                        dist[nrow][ncol] = diff;
+                        pq.push({dist[nrow][ncol], {nrow, ncol}});
                     }
                 }
             }
