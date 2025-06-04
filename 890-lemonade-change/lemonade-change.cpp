e9e6f90d126a4cc776f8af56359787e76bc7df3a
Aryan Biswas <aryanb198@gmail.com>
2025-06-04 21:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (39.34%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,52 +1,8 @@
-// class Solution {
-// public:
-//     bool lemonadeChange(vector<int>& bills) {
-//         map<int, int> mpp;
-//         for(auto it: bills) {
-//             if(it == 5) {
-//                 mpp[it]++;
-//             }
-//             else if(it == 10) {
-//                 if(mpp[5] > 0) {
-//                     mpp[5]--;
-//                     mpp[it]++;
-//                 }
-//                 else{
-//                     return false;
-//                 }
-//             }
-//             else{
-//                 if(mpp[10] > 0) {
-//                     if(mpp[5] > 0) {
-//                         mpp[5]--;
-//                         mpp[10]--;
-//                         mpp[it]++;
-//                     }
-//                     else{
-//                         return false;
-//                     }
-//                 }
-//                 else if(mpp[5] >= 3) {
-//                     mpp[5] -= 3;
-//                     mpp[it]++;
-//                 }
-//                 else{
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-// };
-
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         
-        // space complexity optimised
-        // Instead of map, we'll use variables to track the cnt of each note
         int five = 0, ten = 0, twenty = 0;
-
         for(auto it: bills) {
             if(it == 5) {
                 five++;
@@ -60,16 +16,11 @@ public:
                     return false;
                 }
             }
-            else{
-                if(ten > 0) {
-                    if(five > 0) {
-                        ten--;
-                        five--;
-                        twenty++;
-                    }
-                    else{
-                        return false;
-                    }
+            else if(it == 20) {
+                if(five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                    twenty++;
                 }
                 else if(five >= 3) {
                     five -= 3;
