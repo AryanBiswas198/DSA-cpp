f5b26085388a1cff8010c518489f95ed7b902f44
Aryan Biswas <aryanb198@gmail.com>
2025-06-07 15:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.57%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findUnique(int row, int col, vector<vector<int>> &dp) {
+    int f(int row, int col, vector<vector<int>> &dp) {
         if(row < 0 || col < 0) {
             return 0;
         }
@@ -13,35 +13,15 @@ public:
             return dp[row][col];
         }
 
-        int left = findUnique(row, col-1, dp);
-        int up = findUnique(row-1, col, dp);
+        int left = f(row, col-1, dp);
+        int up = f(row-1, col, dp);
 
-        return dp[row][col] = (left + up);
+        return dp[row][col] = left + up;
     }
 
     int uniquePaths(int m, int n) {
-        // Tabulation
+        // Memoization
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                }
-                else{
-                    int left = 0, up = 0;
-                    if(i > 0) {
-                        up += dp[i-1][j];
-                    }
-
-                    if(j > 0) {
-                        left += dp[i][j-1];
-                    }
-
-                    dp[i][j] = left + up;
-                }
-            }
-        }
-        return dp[m-1][n-1];
+        return f(m-1, n-1, dp);
     }
 };
