0e114e86808b8a5768ca68ddbda5e655b46d58c2
Aryan Biswas <aryanb198@gmail.com>
2025-06-07 13:08:55 +0530

    Time: 3 ms (29.38%) | Memory: 28.2 MB (23.94%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,14 +4,13 @@ public:
         
         int n = nums.size(), target = n/2;
         unordered_map<int, int> mpp;
-        
-        for(auto it: nums){
-            mpp[it]++;
-            
-            if(mpp[it] > target){
+
+        for(auto it: nums) {
+            ++mpp[it];
+            if(mpp[it] > target) {
                 return it;
             }
         }
-        return -1;
+        return 0;
     }
 };
