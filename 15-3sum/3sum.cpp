27aae8ca087d64c232484da3a05f4865588ed78a
Aryan Biswas <aryanb198@gmail.com>
2025-06-08 15:56:30 +0530

    Time: 43 ms (86.66%) | Memory: 29.1 MB (52.45%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,31 +5,33 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
+
         for(int i=0; i<n; i++) {
             if(i != 0 && nums[i] == nums[i-1]) {
                 continue;
             }
+
             int j = i+1, k = n-1;
             while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0) {
-                    j++;
-                }
-                else if(sum > 0) {
-                    k--;
-                }
-                else{
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    ans.push_back(temp);
+                int target = nums[i] + nums[j] + nums[k];
+                if(target == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
 
                     while(j < k && nums[j] == nums[j-1]) {
                         j++;
                     }
+
                     while(j < k && nums[k] == nums[k+1]) {
                         k--;
                     }
+                }
+                else if(target < 0) {
+                    j++;
+                }
+                else{
+                    k--;
                 }
             }
         }
