517c076be6bf06c737125233ac7f99f34646d90b
Aryan Biswas <aryanb198@gmail.com>
2025-06-09 14:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.95%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,52 +11,44 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *ansHead = NULL, *ansTail = NULL;
         
-        if(list1 == NULL){
+        ListNode *ansHead = new ListNode(-1), *ansTail = ansHead;
+        if(list1 == NULL) {
             return list2;
         }
-        
-        if(list2 == NULL){
+        if(list2 == NULL) {
             return list1;
         }
-        
-        while(list1 != NULL && list2 != NULL){
-            if(list1->val < list2->val){
-                if(ansHead == NULL){
-                    ansHead = list1;
-                    ansTail = ansHead;
-                }
-                else{
-                    ansTail->next = list1;
-                    ansTail = ansTail->next;
-                }
+
+        while(list1 != NULL && list2 != NULL) {
+            if(list1->val <= list2->val) {
+                ListNode *newNode = new ListNode(list1->val);
+                ansTail->next = newNode;
+                ansTail = ansTail->next;
                 list1 = list1->next;
             }
             else{
-                if(ansHead == NULL){
-                    ansHead = list2;
-                    ansTail = ansHead;
-                }
-                else{
-                    ansTail->next = list2;
-                    ansTail = ansTail->next;
-                }
+                ListNode *newNode = new ListNode(list2->val);
+                ansTail->next = newNode;
+                ansTail = ansTail->next;
                 list2 = list2->next;
             }
         }
-        
-        while(list1 != NULL){
-            ansTail->next = list1;
+
+        while(list1 != NULL) {
+            ListNode *newNode = new ListNode(list1->val);
+            ansTail->next = newNode;
             ansTail = ansTail->next;
             list1 = list1->next;
         }
-        
-        while(list2 != NULL){
-            ansTail->next = list2;
+
+        while(list2 != NULL) {
+            ListNode *newNode = new ListNode(list2->val);
+            ansTail->next = newNode;
             ansTail = ansTail->next;
             list2 = list2->next;
         }
-        return ansHead;
+        
+        return ansHead->next;
     }
 };
