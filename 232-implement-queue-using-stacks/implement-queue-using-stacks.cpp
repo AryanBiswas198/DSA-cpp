072c89ee792ceb2eee507f34f1b5527d703e6d62
Aryan Biswas <aryanb198@gmail.com>
2025-06-29 20:18:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.74%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -2,50 +2,49 @@ class MyQueue {
 public:
     stack<int> s1;
     stack<int> s2;
-    MyQueue() {}
-
-    void push(int x) { s1.push(x); }
-
-    int pop() {
-        if (s1.empty()) {
-            return -1;
-        } else {
-            while (!s1.empty()) {
-                auto top = s1.top();
-                s1.pop();
-                s2.push(top);
-            }
-
-            int val = s2.top();
-            s2.pop();
-
-            while (!s2.empty()) {
-                auto top = s2.top();
-                s2.pop();
-                s1.push(top);
-            }
-            return val;
-        }
+    MyQueue() {
+        
     }
-
-    int peek() {
-        while (!s1.empty()) {
-            auto top = s1.top();
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        while(!s1.empty()) {
+            int top = s1.top();
             s1.pop();
             s2.push(top);
         }
 
         int val = s2.top();
+        s2.pop();
 
-        while (!s2.empty()) {
-            auto top = s2.top();
+        while(!s2.empty()) {
+            s1.push(s2.top());
             s2.pop();
-            s1.push(top);
         }
         return val;
     }
+    
+    int peek() {
+        while(!s1.empty()) {
+           s2.push(s1.top());
+           s1.pop();
+        }
 
-    bool empty() { return s1.empty(); }
+        int val = s2.top();
+
+        while(!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return val;
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
 };
 
 /**
