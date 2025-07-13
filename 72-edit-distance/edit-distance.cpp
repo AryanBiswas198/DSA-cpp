b9e8f4f6e71a7f3c99fc8c89fcfd097dd063ac2f
Aryan Biswas <aryanb198@gmail.com>
2025-07-13 20:00:25 +0530

    Time: 10 ms (34.35%) | Memory: 13.1 MB (45.93%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
-    int f(int ind1, int ind2, string &word1, string &word2, vector<vector<int>> &dp) {
-        if(ind1 == 0) {
-            return ind2;
-        }
+    // int f(int ind1, int ind2, string &word1, string &word2, vector<vector<int>> &dp) {
+    //     if(ind1 == 0) {
+    //         return ind2;
+    //     }
 
-        if(ind2 == 0) {
-            return ind1;
-        }
+    //     if(ind2 == 0) {
+    //         return ind1;
+    //     }
 
-        if(dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
-        }
+    //     if(dp[ind1][ind2] != -1) {
+    //         return dp[ind1][ind2];
+    //     }
 
-        if(word1[ind1-1] == word2[ind2-1]) {
-            return dp[ind1][ind2] = 0 + f(ind1-1, ind2-1, word1, word2, dp);
-        }
-        else{
-            return dp[ind1][ind2] = 1 + min(f(ind1, ind2-1, word1, word2, dp), min(f(ind1-1, ind2, word1, word2, dp), f(ind1-1, ind2-1, word1, word2, dp)));
-        }
-    }
+    //     if(word1[ind1-1] == word2[ind2-1]) {
+    //         return dp[ind1][ind2] = 0 + f(ind1-1, ind2-1, word1, word2, dp);
+    //     }
+    //     else{
+    //         return dp[ind1][ind2] = 1 + min(f(ind1, ind2-1, word1, word2, dp), min(f(ind1-1, ind2, word1, word2, dp), f(ind1-1, ind2-1, word1, word2, dp)));
+    //     }
+    // }
 
     int minDistance(string word1, string word2) {
         
-        // Memoization
+        // Tabulation
         int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        return f(n, m, word1, word2, dp);
+        for(int i=0; i<=m; i++) {
+            dp[0][i] = i;
+        }
+
+        for(int i=0; i<=n; i++) {
+            dp[i][0] = i;
+        }
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
