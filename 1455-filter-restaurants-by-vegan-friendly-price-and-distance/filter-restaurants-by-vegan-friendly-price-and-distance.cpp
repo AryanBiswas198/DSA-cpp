03250ccb1c2c87a78fd88bd3067dd8cf8c0c0529
Aryan Biswas <aryanb198@gmail.com>
2025-07-30 16:39:40 +0530

    Time: 1 ms (69.23%) | Memory: 35.2 MB (21.54%) - LeetSync

--- 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.cpp
+++ 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<int> filterRestaurants(vector<vector<int>>& restaurants, int veganFriendly, int maxPrice, int maxDistance) {
         
-        int n = restaurants.size();
+        vector<int> ans;
         priority_queue<pair<int, int>> pq;
 
         for(auto it: restaurants) {
             if(it[3] <= maxPrice && it[4] <= maxDistance) {
-                if(veganFriendly == 1) {
-                    if(it[2] == 1) {
-                        pq.push({it[1], it[0]});
-                    }
-                }
-                else if(veganFriendly == 0) {
+                if((veganFriendly == 1 && it[2] == 1) || (veganFriendly == 0)) {
                     pq.push({it[1], it[0]});
                 }
             }
         }
 
-        vector<int> ans;
         while(!pq.empty()) {
             auto top = pq.top();
             pq.pop();
