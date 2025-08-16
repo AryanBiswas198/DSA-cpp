ed9bd52d1cbd61ae3aef0a79207a9218b4d81272
Aryan Biswas <aryanb198@gmail.com>
2025-08-16 20:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (12.18%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maximum69Number (int num) {
         
-        string numm = to_string(num);
-        for(char &c: numm) {
+        string number = to_string(num);
+        for(auto &c: number) {
             if(c == '6') {
                 c = '9';
                 break;
             }
         }
-        return stoi(numm);
+        return stoi(number);
     }
 };
